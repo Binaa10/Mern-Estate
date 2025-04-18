5f142f688e94174e8e696c5e7222db93b4a16444
Abyalew <biniosbinios2@gmail.com>
2025-04-18 11:57:16 +0300

    Complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,14 +2,27 @@ import React, { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import { storage } from "../appWrite/appwriteConfig.js";
 import { ID, Permission, Role } from "appwrite";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
 
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
   const [uploadProgress, setUploadProgress] = useState(0);
   const [uploadComplete, setUploadComplete] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  //console.log(formData);
+  //console.log(file);
+  //console.log(currentUser);
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -31,35 +44,64 @@ export default function Profile() {
       const fileId = ID.unique();
 
       const response = await storage.createFile(
-        "67fed0b9001c2550df97", // bucket ID
+        "67fed0b9001c2550df97",
         fileId,
         file,
-        [
-          Permission.read(Role.any()), // Public access for now
-          Permission.update(Role.any()),
-        ]
+        [Permission.read(Role.any()), Permission.update(Role.any())]
       );
 
-      // Show uploaded image
       const previewUrl = storage.getFileView(
         "67fed0b9001c2550df97",
         response.$id
       );
+
       setImageUrl(previewUrl);
+      const updatedData = { ...formData, avatar: previewUrl }; // ✅ fix here
+      setFormData(updatedData);
       setUploadComplete(true);
     } catch (error) {
       console.error("Upload failed:", error);
     }
   };
 
-  console.log(imageUrl);
+  //console.log(imageUrl);
+  console.log(formData);
+
+  const handleChange = (e) => {
+    const updatedData = { ...formData, [e.target.id]: e.target.value };
+    setFormData(updatedData);
+    //console.log(updatedData); // 👈 Logs full object
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify(formData), // ✅ fix typo
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
 
   return (
     <div>
       <div className="max-w-lg mx-auto p-3 ">
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <input
             onChange={handleFileChange}
             type="file"
@@ -93,26 +135,34 @@ export default function Profile() {
           )}
 
           <input
+            defaultValue={currentUser.username}
             id="username"
             type="text"
             placeholder="username"
             className="border p-3 rounded-lg"
+            onChange={handleChange}
           />
           <input
+            defaultValue={currentUser.email}
             id="email"
             type="email"
             placeholder="email"
             className="border p-3 rounded-lg"
+            onChange={handleChange}
           />
           <input
             id="password"
             type="password"
             placeholder="password"
             className="border p-3 rounded-lg"
+            onChange={handleChange}
           />
 
-          <button className="bg-slate-800 p-3 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">
-            Update
+          <button
+            disabled={loading}
+            className="bg-slate-800 p-3 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Update"}
           </button>
         </form>
 
@@ -120,6 +170,10 @@ export default function Profile() {
           <span className="text-red-800 cursor-pointer">Delete account</span>
           <span className="text-red-800 cursor-pointer">Sign out</span>
         </div>
+        <p className="text-red-700 mt-5">{error ? error : ""}</p>
+        <p className="text-green-700 mt-4">
+          {updateSuccess ? "user is updated successfully!" : ""}
+        </p>
       </div>
     </div>
   );
