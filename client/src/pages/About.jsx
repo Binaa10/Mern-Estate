d7eb08fae3e61ee3abd79afb2edeca8230cd3c90
Abyalew <biniosbinios2@gmail.com>
2025-05-02 11:18:49 +0300

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function About() {
-  return <div>about</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="font-bold text-3xl mb-4 text-slate-800">
+        About Binios Estate
+      </h1>
+      <p className="mb-4 text-slate-700 ">
+        BiniosEstate is a trusted real estate agency that specializes in helping
+        clients buy, sell, and rent properties across vibrant and sought-after
+        neighborhoods. Our team of skilled and passionate agents is committed to
+        delivering outstanding service and ensuring that every step of your real
+        estate journey is seamless and stress-free.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Our mission is to help our clients reach their real estate goals through
+        expert guidance, tailored support, and in-depth knowledge of the local
+        market. Whether you're planning to purchase, sell, or lease a property,
+        we are here to walk you through the entire process with confidence and
+        care.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        With years of experience and a strong understanding of the real estate
+        landscape, our team brings a high level of professionalism and
+        dedication to every transaction. At BiniosEstate, we believe real estate
+        should be exciting, empowering, and rewarding—and we are devoted to
+        turning that belief into a reality for every client we serve.
+      </p>
+    </div>
+  );
 }
